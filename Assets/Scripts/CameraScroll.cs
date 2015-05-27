3e2a039ffc6976850ba7dfcbec0f046cf42fc8ca
Francisco Carrera <fc1159@nyu.edu>
2015-05-27 01:02:41 -0400

    Added sample path to prototype level and simple awsd scrolling for the camera

---
+++ Assets/Scripts/CameraScroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScroll : MonoBehaviour {
+
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+	void Update() {
+		float translation = Input.GetAxis("Vertical") * speed;
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate(Vector3.forward * translation);
+		transform.Rotate(0, rotation, 0, Space.World);
+	}
+}
