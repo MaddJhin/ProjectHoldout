5d977c168ef211dd2dc9515567d071149c5329d1
Francisco Carrera <fc1159@nyu.edu>
2015-06-03 10:06:56 -0400

    Added Individual test spawner with some test enemies for level design

---
+++ Assets/Scripts/SimpleTestSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleTestSpawner : MonoBehaviour {
+
+	public GameObject enemy;                // The enemy prefab to be spawned.
+	public float spawnTime = 3f;            // How long between each spawn.
+		
+	void Start ()
+	{
+		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+	
+	
+	void Spawn ()
+	{	
+		// Create an instance of the enemy prefab at the spawn point's position and rotation.
+		Instantiate (enemy, transform.position, transform.rotation);
+	}
+}
