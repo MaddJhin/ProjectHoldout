bf3b88cc30277a483e85236112849636453b0527
AriiMoose <ariimoose@gmail.com>
2015-06-06 17:30:09 +0100

    Added Flying Unit w/ prefab

---
+++ Assets/Scripts/UnitTypes/FlyingUnit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingUnit : MonoBehaviour 
+{
+
+    [SerializeField]
+    public GameObject targetLoc;
+    public float height = 30f;
+
+    private Rigidbody rb;
+    private NavMeshAgent agent;
+    private Animator anim;
+    private UnitStats stats;
+    private UnitSight vision;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        stats = GetComponent<UnitStats>();
+        vision = GetComponent<UnitSight>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, height, transform.position.z);
+        Move();
+    }
+
+    void Move()
+    {
+        agent.Resume();
+        agent.SetDestination(targetLoc.transform.position);
+    }
+}
