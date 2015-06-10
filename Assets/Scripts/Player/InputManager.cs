acb37c8054a802241801d6dde19eb53bdf918796
Francisco Carrera <fc1159@nyu.edu>
2015-06-10 12:54:39 -0400

    Created simplified player controls. Now possible to select player character to move. Moving restricted to objects tagged "Barricade"

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public float surfaceOffset = 1.5f;
+	public PlayerCharacterControl setTargetOn;
+
+	void Update () {
+		// Run when user clicks
+		if (!Input.GetMouseButtonDown(0))
+		{
+			return;
+		}
+
+		// Raycast to mouse
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		// Return if nothing was hit
+		if (!Physics.Raycast(ray, out hit))
+		{
+			return;
+		}
+
+		// Grab the player script component of the player selected
+		if (hit.collider.tag == "Player")
+		{
+			setTargetOn = hit.transform.gameObject.GetComponent<PlayerCharacterControl>();
+		}
+
+		// If player selects barricade, call SetTarget on the selected player character, checking for null.
+		else if (hit.collider.tag == "Barricade" && setTargetOn != null)
+		{
+			setTargetOn.SetTarget(hit.point);
+		}
+
+		// Place target reticule 
+		//transform.position = hit.point + hit.normal*surfaceOffset;
+	}
+}
---
+++ Assets/Scripts/Player/PlayerCharacterControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (NavMeshAgent))]
+
+public class PlayerCharacterControl : MonoBehaviour {
+
+	public Transform target; // target to aim for
+	public NavMeshAgent agent { get; private set; }
+
+	Animator m_Animator;
+	float m_TurnAmount;
+	float m_ForwardAmount;
+	
+	// Use this for initialization
+	void Start () {
+		agent = GetComponentInChildren<NavMeshAgent>();
+		m_Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		UpdateAnimator(agent.desiredVelocity);
+	}
+
+	public void SetTarget(Vector3 target)
+	{
+		agent.SetDestination(target);
+	}
+
+	void UpdateAnimator(Vector3 move)
+	{
+		//Set float values based on nav agent velocity
+		if (move.magnitude > 1f) move.Normalize();
+		move = transform.InverseTransformDirection(move);
+		m_TurnAmount = Mathf.Atan2(move.x, move.z);
+		m_ForwardAmount = move.z;
+
+		// Update animator float values 
+		m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
+		m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
+	}
+}
+	
