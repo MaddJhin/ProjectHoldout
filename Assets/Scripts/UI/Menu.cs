2a6f846862c122275923ae8740ee96c600bc1e9c
Francisco Carrera <fc1159@nyu.edu>
2015-08-21 20:23:41 -0400

    == General == - Added Animation controllers to loadout menu test scene

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* USAGE:
+ * ======================================
+ * Used to pass the IsOpen value to menu panels
+ * Sets if menu panels are open, interactable 
+ * or block raycasts
+ * ======================================
+ * 
+ * Date Created: 21 Aug 2015
+ * Last Modified: 21 Aug 2015
+ * Authors: Francisco Carrera
+ */
+
+[RequireComponent(typeof (Animator))]
+[RequireComponent(typeof (CanvasGroup))]
+
+public class Menu : MonoBehaviour {
+
+	private Animator animator;
+	private CanvasGroup canvasGroup;
+
+	// Get Set bool IsOpen
+	public bool IsOpen
+	{
+		get { return animator.GetBool("IsOpen"); }
+		set { animator.SetBool("IsOpen", value); }
+	}
+	
+	public void Awake(){
+		animator = GetComponent<Animator>();
+		canvasGroup = GetComponent<CanvasGroup>();
+
+		// Set offset on awake to zero, allows menues to be moved in editor
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2 (0, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// If the state is open, make menu interactable / block raycasts 
+		if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+		{
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = true;
+		}
+		// Otherwise, make it not interactable / block raycasts
+		else
+		{
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = false;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* USES:
+ * ==============
+ * Menu.cs
+ * ==============
+ * 
+ * USAGE:
+ * ======================================
+ * Used to call method to open menus through 
+ * UI buttons. UI button calls the method and 
+ * the script sets the animator trigger bools
+ * ======================================
+ * 
+ * Date Created: 21 Aug 2015
+ * Last Modified: 21 Aug 2015
+ * Authors: Francisco Carrera
+ */
+
+public class MenuManager : MonoBehaviour {
+
+	public Menu currentMenu;
+
+	private int clickableMask; 	// A layer mask so the raycast only hits UI elements 
+
+
+	void Awake (){
+		clickableMask = LayerMask.GetMask("UI");
+	}
+
+	void Start () {
+		// Open Starting Menu
+		ShowMenu(currentMenu);
+	}
+	
+//	void Update (){
+//		if (Input.GetKeyDown (KeyCode.Mouse0))
+//		{
+//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//
+//			if (!Physics.Raycast(ray, 1000f, clickableMask))
+//			    {
+//					currentMenu = null;
+//					ShowMenu(currentMenu);
+//				}
+//		}
+//	}
+
+	// used to open new menus. Called by buttons
+	public void ShowMenu (Menu menu){
+		// Close any current menus by setting IsOpen to false
+		if (currentMenu != null) currentMenu.IsOpen = false;
+
+		// Set new menu and set IsOpen to true
+		if (currentMenu != null)
+		{
+			currentMenu = menu;
+			currentMenu.IsOpen = true;
+		}
+	}
+}
