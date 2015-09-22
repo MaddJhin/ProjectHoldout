2c5112c6b1a079a19ae084ebe527800308b94e09
MaddJhin <fc1159@nyu.edu>
2015-09-22 14:34:25 -0400

    == UI == - Hooked up world selector UI in WorldSelectTestScene to mission scenes. - First mission of every planet starts when the launch button is pressed after selecting the relevant planet.

---
+++ Assets/Scripts/UI/WorldMenuManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/* USES:
+ * ==============
+ * Menu.cs
+ * ==============
+ * 
+ * USAGE:
+ * ======================================
+ * Used to call method to open menus through 
+ * UI buttons. UI button calls the method and 
+ * the script sets the animator trigger bools
+ * ======================================
+ * 
+ * Date Created: 21 Aug 2015
+ * Last Modified: 21 Aug 2015
+ * Authors: Francisco Carrera
+ */
+
+public class WorldMenuManager : MonoBehaviour
+{
+
+    public Menu planetMenu;
+    public Button launchButton;
+
+    private Menu currentMenu;
+
+    void Start()
+    {
+        currentMenu = planetMenu;
+        // Open Starting Menu
+        ShowMenu(currentMenu);
+    }
+
+    // used to open new menus. Called by buttons
+    public void ShowMenu(Menu menu)
+    {
+        // Close any current menus by setting IsOpen to false
+        if (currentMenu != null) currentMenu.IsOpen = false;
+
+        // Set new menu and set IsOpen to true
+        currentMenu = menu;
+        currentMenu.IsOpen = true;
+    }
+
+    public void SetLevelToLoad(int parameter)
+    {
+        launchButton.onClick.RemoveAllListeners();
+        launchButton.onClick.AddListener(delegate { LoadLevel(parameter); });
+    }
+
+    void LoadLevel(int lvlIndex) {
+        Application.LoadLevel(lvlIndex);
+    }
+}
